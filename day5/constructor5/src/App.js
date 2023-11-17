e43b026feb2748d94c7968531fdf8f31f7947900
akshu-13 <106052965+akshu-13@users.noreply.github.com>
2023-11-17 11:45:50 +0530

    Day5

---
+++ day5/constructor5/src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import Funlifecycle from './components/Funlifecycle';
+import Lifecycle from './components/Lifecycle';
+
+function App() {
+  return (
+    <div className="App">
+      <Lifecycle/>
+      <Funlifecycle/>
+    </div>
+  );
+}
+
+export default App;
---
+++ day5/constructor5/src/components/Funlifecycle.js
@@ -0,0 +1,14 @@
+import { useEffect, useState } from "react";
+const Funlifecycle=()=>{
+    const[count,setCount]=useState(0);
+    useEffect(()=>{
+        console.log("Component is mounted");
+        return()=>{
+            console.log("Component will unmount");
+        };
+    }, {});
+    useEffect(()=>{
+        console.log("Component has been updated");
+    })
+}
+export default Funlifecycle;
